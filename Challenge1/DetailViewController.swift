73e0a1b04a8844a87f333e5cc41dff47100ff412
o-pylypiv <bassenia@gmail.com>
2024-06-04 11:35:55 +0300

    basic functionality

---
+++ Challenge1/DetailViewController.swift
@@ -0,0 +1,85 @@
+//
+//  DetailViewController.swift
+//  Challenge1
+//
+//  Created by Olha Pylypiv on 10.01.2024.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var flags = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = (selectedImage as? NSString)?.deletingPathExtension.uppercased()
+        //title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        
+        if let imageToLoad = selectedImage {
+            if let image = UIImage(named: imageToLoad) {
+                imageView.image = image.stroked
+                
+            }
+            
+        }
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else
+        {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image, title ?? "Flag"], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension UIImage {
+  var stroked: UIImage? {
+     let rect = CGRect(origin: .zero, size: size)
+     UIGraphicsBeginImageContextWithOptions(size, false, 0)
+     defer { UIGraphicsEndImageContext() }
+     let bezierPath = UIBezierPath(roundedRect: rect, cornerRadius: 1)
+     bezierPath.addClip()
+     bezierPath.lineWidth = 1
+     draw(in: rect)
+     guard let context = UIGraphicsGetCurrentContext() else { return nil }
+     context.setStrokeColor(UIColor.lightGray.cgColor)
+     bezierPath.lineWidth = 1
+     bezierPath.stroke()
+     return UIGraphicsGetImageFromCurrentImageContext() }
+}
--- Challenge1/ViewController.swift
+++ Challenge1/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var flags = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "World Flags"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.rowHeight = 65
+        // Set the background color to "Default"
+        tableView.backgroundColor = .systemGroupedBackground
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try!fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix(".png"){
+                flags.append(item)
+            }
+        }
+        print(flags)
+    
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        //cell.textLabel?.text = flags[indexPath.row]
+        cell.imageView?.image = UIImage(named: "\(flags[indexPath.row])")?.stroked
+        
+        //cell.layer.borderWidth = 0.4
+        //cell.layer.borderColor = UIColor.lightGray.cgColor
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = flags[indexPath.row]
+            vc.flags = flags
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    
+    
 
 }
 
